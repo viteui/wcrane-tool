71911963d93069b18ec9e87df56ee16e92087df5
wcrane <1191814251@qq.com>
2024-10-07 21:06:52 +0800

    publish version 1.0.45

--- utils/log.js
+++ utils/log.js
@@ -6,16 +6,16 @@ const chalk = require('chalk')
 
 const logger = {
     success: (...msg) => {
-        console.log.apply(console, [chalk.green('success:'), ...msg])
+        console.log.apply(console, [chalk.green('\nsuccess:'), ...msg])
     },
     error: (...msg) => {
-        console.log.apply(console, [chalk.red('error:'), ...msg])
+        console.log.apply(console, [chalk.red('\nerror:'), ...msg])
     },
     info: (...msg) => {
-        console.log.apply(console, [chalk.blue('info:'), ...msg])
+        console.log.apply(console, [chalk.blue('\ninfo:'), ...msg])
     },
     warn: (...msg) => {
-        console.log.apply(console, [chalk.yellow('warn:'), ...msg])
+        console.log.apply(console, [chalk.yellow('\nwarn:'), ...msg])
     },
 
 }
