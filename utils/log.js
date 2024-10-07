ef681ba83cacbefd48b56a3212b82d8bf0d8c8ad
wcrane <1191814251@qq.com>
2024-10-07 22:00:40 +0800

    publish version 1.0.47

--- utils/log.js
+++ utils/log.js
@@ -4,20 +4,62 @@
 // 引入log模块
 const chalk = require('chalk')
 
+const levels = {
+    error: 0,
+    warn: 1,
+    info: 2,
+    verbose: 4,
+    debug: 5,
+};
+
+
+const getLoggerLevelNum = () => {
+    return levels[logger.level] || levels.debug
+}
+
+
+
+
+
+
 const logger = {
     success: (...msg) => {
-        console.log.apply(console, [chalk.green('\nsuccess:'), ...msg])
+        if (getLoggerLevelNum() >= levels.success) {
+            console.log.apply(console, [chalk.green('\nsuccess:'), ...msg])
+        }
+
     },
     error: (...msg) => {
-        console.log.apply(console, [chalk.red('\nerror:'), ...msg])
+        if (getLoggerLevelNum() >= levels.error) {
+            console.log.apply(console, [chalk.red('\nerror:'), ...msg])
+        }
+
     },
     info: (...msg) => {
-        console.log.apply(console, [chalk.blue('\ninfo:'), ...msg])
+        if (getLoggerLevelNum() >= levels.info) {
+            console.log.apply(console, ['\ninfo:', ...msg])
+        }
     },
     warn: (...msg) => {
-        console.log.apply(console, [chalk.yellow('\nwarn:'), ...msg])
+        if (getLoggerLevelNum() >= levels.warn) {
+            console.log.apply(console, [chalk.yellow('\nwarn:'), ...msg])
+        }
     },
+    debug: (...msg) => {
+        if (getLoggerLevelNum() >= levels.debug) {
+            console.log.apply(console, [chalk.blue('\ndebug:'), ...msg])
+        }
+    },
+    verbose: (...msg) => {
+        if (getLoggerLevelNum() >= levels.verbose) {
+            console.log.apply(console, [chalk.gray('\nverbose:'), ...msg])
+        }
+    }
 
 }
+
+logger.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info'  // 判断debug模式
+
+
 
 module.exports = logger;
