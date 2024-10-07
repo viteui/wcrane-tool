f461d5f2550d7e886b34e398a919ff01c680049a
wcrane <1191814251@qq.com>
2024-10-07 20:49:13 +0800

    publish version 1.0.42

--- command/pkg/publish.js
+++ command/pkg/publish.js
@@ -173,7 +173,7 @@ async function publish() {
         });
     }
     const updateNpm = `${pkgJson.name}/@${pkgJson.version}`
-    console.success(`updated success: \n yarn add ${updateNpm} \n npm i ${updateNpm}`)
+    log.success(`updated success: \n yarn add ${updateNpm} \n npm i ${updateNpm}`)
 }
 
 module.exports = publish;
