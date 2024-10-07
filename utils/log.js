73ae1487d44973f300677732be3659deab295d7c
wcrane <1191814251@qq.com>
2024-10-07 22:05:13 +0800

    publish version 1.0.51

--- utils/log.js
+++ utils/log.js
@@ -25,7 +25,6 @@ const getLoggerLevelNum = () => {
 
 const logger = {
     success: (...msg) => {
-        console.log(getLoggerLevelNum());
         if (getLoggerLevelNum() >= levels.success) {
             console.log.apply(console, [chalk.green('\nsuccess:'), ...msg])
         }
